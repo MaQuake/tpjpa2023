cdb39ec855e2abc306e36bf3c136eea3db83fa11
ayleborgne <aymerick.leborgne@gmail.com>
2023-09-07 19:24:28 +0200

    fix + mermaid

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Template de projet pour le TP JPA 2021 UniR
+
+```mermaid
+    classDiagram
+    User <|-- Professional
+    User "2"--  "*"Appointment
+```
+
+
--- src/main/java/domain/Professional.java
+++ src/main/java/domain/Professional.java
@@ -7,26 +7,16 @@ import jakarta.persistence.Id;
 @Entity
 public class Professional extends User{
 
-    private long id;
     private String profession;
 
     public Professional() {
     }
 
-    @Id
-    @GeneratedValue
-    public long getId() {
-        return id;
-    }
 
     public String getProfession() {
         return profession;
     }
 
-    @Override
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public void setProfession(String profession) {
         this.profession = profession;
